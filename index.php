91823fc6814f1fd8b34099d07672dd8137bad4a8
Ahmad Haikal <haikala0010@gmail.com>
2024-12-06 09:49:24 +0700

    membuat dashboard admin dan form menambah akun

--- index.php
+++ index.php
@@ -1,38 +1,47 @@
-<?php
-if (isset($_GET["page"])) {
-    switch ($_GET["page"]) {
-        case "login":
-            include_once "view/auth/login.php";
-            break;
-        case "register":
-            include_once "view/auth/register.php";
-            break;
-        case "home":
-            include_once "view/home.php";
-            break;
-        case "tambahAkun":
-            include_once "view/superAdmin/createUser.php";
-            break;
-    }
-} else {
-    include_once "view/home.php";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>SIKERMA PNP</title>
 </head>
 
 <body>
-    <a href="?page=home">Home</a>
-    <a href="?page=register">Register</a>
-    <a href="?page=login">Login</a>
-    <a href="?page=tambahAkun">Tambah Akun</a>
+    <div class="container">
+        <?php
+        if (isset($_GET["page"])) {
+            switch ($_GET["page"]) {
+                case "login":
+                    include_once "view/auth/login.php";
+                    break;
+                case "register":
+                    include_once "view/auth/register.php";
+                    break;
+                case "home":
+                    include_once "view/home.php";
+                    break;
+                case "tambahAkun":
+                    include_once "view/superAdmin/createUser.php";
+                    break;
+                case "mou_moa":
+                    include_once "view/superAdmin/dashboardSuperAdmin.php";
+                    break;
+            }
+        } else {
+            include_once "view/home.php";
+        }
+
+        ?>
+        <a href="?page=home">Home</a>
+        <a href="?page=register">Register</a>
+        <a href="?page=login">Login</a>
+        <a href="?page=tambahAkun">Tambah Akun</a>
+        <a href="?page=mou_moa">List MOU MOA</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ view/superAdmin/dashboardSuperAdmin.php
@@ -0,0 +1,51 @@
+<?php
+include_once "database/koneksi.php";
+global $pdo;
+?>
+
+<h2>List Data MOU dan MOA</h2>
+<table id="tabel-mou-moa" class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Jenis Kerjasama</th>
+            <th>Mitra</th>
+            <th>Keterangan</th>
+            <th>Jurusan</th>
+            <th>Dokumen</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $i = 1;
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM tb_mou_moa JOIN tb_mitra ON tb_mou_moa.mitra_idMitra = tb_mitra.idMitra");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Gagal mengambil data: " . $e->getMessage();
+        }
+
+        foreach ($result as $row):
+        ?>
+            <tr>
+                <td><?= $i ?></td>
+                <td><?= $row["jenisKerjasama"] ?></td>
+                <td><?= $row["namaInstansi"] ?></td>
+                <td><?= $row["keterangan"] ?></td>
+                <td><?= $row["jurusan"] ?></td>
+                <td><?= $row["fileDokumen"] ?></td>
+                <td>
+                    <a href=""><i class="btn btn-primary bi bi-eye"></i></a>
+                    <a href=""><i class="btn btn-warning bi bi-pencil-square"></i></a>
+                    <a href=""><i class="btn btn-danger bi bi-trash"></i></a>
+                </td>
+            </tr>
+
+        <?php
+            $i++;
+        endforeach;
+        ?>
+    </tbody>
+</table>
