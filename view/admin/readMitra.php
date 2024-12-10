9dc9d29f5b59b08d4f2f820457116a0c388a52e8
Ahmad Haikal <haikala0010@gmail.com>
2024-12-10 19:47:08 +0700

    membuat detail mitra

--- view/admin/readMitra.php
+++ view/admin/readMitra.php
@@ -61,12 +61,40 @@ if ($_SESSION["role"] === "super admin" || $_SESSION["role"] === "admin") {
                     <td><?= $row["provinsi"] ?></td>
                     <td>
                         <form action="" method="post" class="action-buttons-vertical">
-                            <a href="" class="button-no-border tombol-aksi" data-id="<?= $row["idMitra"] ?>"><i class="btn btn-primary bi bi-list-ul"></i></a>
+                            <button type="button" name="detail" class="button-no-border tombol-aksi" data-bs-toggle="modal" data-bs-target="#<?= $row["idMitra"] ?>">
+                                <i class="btn btn-primary bi bi-list-ul"></i>
+                            </button>
                             <a href="index.php?action=update_mitra&idMitra=<?= $row["idMitra"] ?>"><i class="btn btn-warning bi bi-pencil-square"></i></a>
                             <button name="delete" class="button-no-border tombol-aksi" value="<?= $row["idMitra"] ?>" onclick="return confirm('Apakah anda yakin ingin menghapus?')"><i class="btn btn-danger bi bi-trash"></i></button>
                         </form>
                     </td>
                 </tr>
+
+                <!-- Modal -->
+                <div class="modal fade" id="<?= $row["idMitra"] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel"><?= $row["namaInstansi"] ?></h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <h6>Nama Pimpinan: <?= $row["namaPimpinan"] ?></h6>
+                                <h6>Alamat Mitra: <?= $row["alamatMitra"] ?></h6>
+                                <h6>Email Mitra: <?= $row["emailMitra"] ?></h6>
+                                <h6>Telepon Mitra: <?= $row["teleponMitra"] ?></h6>
+                                <h6>Bidang Usaha: <?= $row["bidangUsaha"] ?></h6>
+                                <h6>Website Mitra: <?= $row["websiteMitra"] ?></h6>
+                                <h6>Provinsi: <?= $row["provinsi"] ?></h6>
+                                <h6>Kota: <?= $row["kota"] ?></h6>
+                            </div>
+                            <div class="modal-footer">
+                                <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Understood</button> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             <?php
                 $i++;
